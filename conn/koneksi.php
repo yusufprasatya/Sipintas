e4f05c7648862d214047586c78e387ffda87fa55
Yusuf Prasatya <38418797+yusufprasatya@users.noreply.github.com>
2021-10-07 22:48:15 +0700

    Delete conn directory

--- conn/koneksi.php
+++
@@ -1,8 +0,0 @@
- <?php
-    $koneksi = mysqli_connect("localhost", "root", "123456", "sipintas2");
-    function rupiah($angka)
-    {
-
-        $hasil_rupiah = "Rp " . number_format($angka, 2, ',', '.');
-        return $hasil_rupiah;
-    }
